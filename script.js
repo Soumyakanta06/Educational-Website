3a8d762902a6ccd6dfe2a7ef5e90df3aea01e67c
Soumyakanta Mohanty <soumyakantam022@gmail.com>
2021-08-09 23:20:07 +0530

    js added

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ $(document).ready(function() {
   }
 
  });
+
+ learners = document.getElementById("learners");
+ courses = document.getElementById("courses");
+ instructors = document.getElementById("instructors");
+
+var a = 1;
+setInterval(() => {
+  if(a<=4000){
+    learners.innerHTML = a +"+";
+    a++;
+
+  }
+},10)
+
+var b = 1;
+setInterval(() => {
+  if(b<=30){
+    courses.innerHTML = b + "+";
+    b++;
+
+  }
+},50)
+
+var c = 1;
+setInterval(() => {
+  if(c<=80){
+    instructors.innerHTML = c + "+";
+    c++;
+
+  }
+},50)
